801ad93418a7d71e757e4540175867a33fc8bb0f
Lucas Bartholemy <lucas@bartholemy.com>
2014-10-03 06:04:44 +0200

    Updated translations (translate.humhub.org)

--- messages/tr/models_Message.php
+++ messages/tr/models_Message.php
@@ -1,5 +1,5 @@
 <?php
 return array (
   'New message from {senderName}' => 'Yeni mesaj var. Gönderen {senderName}',
-  'and {counter} other users' => 've {counter} diğer kullanıcı',
+  'and {counter} other users' => 've {counter} diğer kullanıcılar',
 );
