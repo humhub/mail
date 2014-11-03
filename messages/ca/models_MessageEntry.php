3f9487742308a610344e571e6c485c0ece10a44a
Lucas Bartholemy <lucas@bartholemy.com>
2014-11-03 19:26:42 +0100

    Added catalan language files

--- messages/ca/models_MessageEntry.php
+++ messages/ca/models_MessageEntry.php
@@ -17,9 +17,5 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  'Copyright © 2013 by humhub' => '@@Copyright © 2013 by humhub.@@',
-  'Email preferences' => '@@电子邮件偏好@@',
-  'New message' => '@@新消息@@',
-  'Reply now' => '@@现在回复@@',
-  'send you a new message in' => '发送给你一个新消息',
+  'New message in discussion from %displayName%' => '',
 );
