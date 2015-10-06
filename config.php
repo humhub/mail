a4a331f5236a0047bdebe218a54c99cdce32f871
andystrobel <andreas.strobel@googlemail.com>
2015-10-06 11:40:33 +0200

    Fixed: Deleted user crashes #52

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ return [
     'class' => 'humhub\modules\mail\Module',
     'namespace' => 'humhub\modules\mail',
     'events' => [
-        ['class' => User::className(9), 'event' => User::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\mail\Events', 'onUserDelete']],
+        ['class' => User::className(), 'event' => User::EVENT_BEFORE_DELETE, 'callback' => ['humhub\modules\mail\Events', 'onUserDelete']],
         ['class' => TopMenu::className(), 'event' => TopMenu::EVENT_INIT, 'callback' => ['humhub\modules\mail\Events', 'onTopMenuInit']],
         ['class' => NotificationArea::className(), 'event' => NotificationArea::EVENT_INIT, 'callback' => ['humhub\modules\mail\Events', 'onNotificationAddonInit']],
         ['class' => ProfileHeaderControls::className(), 'event' => ProfileHeaderControls::EVENT_INIT, 'callback' => ['humhub\modules\mail\Events', 'onProfileHeaderControlsInit']],
