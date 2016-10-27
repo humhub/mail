28f690b3b7ded2df1489044d99ecdee483a5f00f
xtc <3cst4sy@users.noreply.github.com>
2016-10-27 12:16:17 +0200

    message preview with all recipients

--- views/mail/_messagePreview.php
+++ views/mail/_messagePreview.php
@@ -16,11 +16,20 @@ $message = $userMessage->message;
 <?php if ($message->getLastEntry() != null) : ?>
     <li class="messagePreviewEntry_<?php echo $message->id; ?> messagePreviewEntry entry">
         <a href="javascript:loadMessage('<?php echo $message->id; ?>');">
-            <div class="media">
-                <img class="media-object img-rounded pull-left" data-src="holder.js/32x32" alt="32x32" style="width: 32px; height: 32px;" src="<?php echo $message->getLastEntry()->user->getProfileImage()->getUrl(); ?>">
+            <div>
+                <h5><b><?php echo Html::encode(Helpers::truncateText($message->title, 75)); ?></b></h5>
+                <p>
+                    <?php foreach ($message->users as $user) : ?>
+                        <?php if ($user->id == Yii::$app->user->id) { continue; } ?>
+                        <img src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+                             class="img-rounded tt img_margin"
+                             data-src="holder.js/32x32" alt="32x32" style="width: 32px; height: 32px;"
+                             data-toggle="tooltip" data-placement="top" title=""
+                             data-original-title="<?php echo Html::encode($user->displayName); ?>">
+                         <?php endforeach; ?>
+                </p>
                 <div class="media-body text-break">
-                    <h4 class="media-heading"><?php echo Html::encode($message->getLastEntry()->user->displayName); ?> <small><?php echo TimeAgo::widget(['timestamp' => $message->updated_at]); ?></small></h4>
-                    <h5><?php echo Html::encode(Helpers::truncateText($message->title, 75)); ?></h5>
+                    <h5 class="media-heading"><?php echo Yii::t('MailModule.views_mail_index', 'Last reply by: ') . Html::encode($message->originator->displayName); ?> <small><?php echo TimeAgo::widget(['timestamp' => $message->updated_at]); ?></small></h5>
                     <?php echo Helpers::truncateText(MarkdownView::widget(['markdown' => $message->getLastEntry()->content, 'parserClass' => '\humhub\libs\MarkdownPreview', 'returnPlain' => true]), 200); ?>
                     <?php
                     // show the new badge, if this message is still unread
