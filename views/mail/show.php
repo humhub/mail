d69483e151a1b813ae74d62dbcf44a341fa9166f
xtc <3cst4sy@users.noreply.github.com>
2017-03-14 23:50:03 +0100

    Scroll to the newst message Added msg_{ID} to every message displayed Added "Back To Top"-Button

--- views/mail/show.php
+++ views/mail/show.php
@@ -149,6 +149,12 @@ use humhub\compat\CActiveForm;
 
                 <?php CActiveForm::end(); ?>
             </div>
+            
+            
+<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+</a>
+
         <?php } ?>
 
     </div>
