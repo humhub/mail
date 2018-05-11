022d1736c3d5a3775106f62721060987a16a493e
Daniel Kesselberg <mail@danielkesselberg.de>
2018-05-11 18:27:04 +0200

    Update test configuration

--- tests/config/common.php
+++ tests/config/common.php
@@ -2,9 +2,4 @@
 /**
  * This config is shared by all suites (unit/functional/acceptance) and can be overwritten by a suite config (e.g. functional.php)
  */
-return [
-    'params' => [
-        'moduleAutoloadPaths' => ['D:\codebase\humhub\modules'],
-        'allowedLanguages' => ['en']
-    ]
-];
+return [];
