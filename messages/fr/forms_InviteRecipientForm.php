db7e437871f4348a8a17bc46ce9eb435c72514b7
HumHub Translations <info@humhub.com>
2018-09-18 11:23:47 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/fr/forms_InviteRecipientForm.php
+++ messages/fr/forms_InviteRecipientForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Recipient' => 'Destinataire',
-  'User {name} is already participating!' => 'L\'utilisateur {name} participe déjà !',
+  'User {name} is already participating!' => 'L\'utilisateur {name} participe déjà.',
   'You are not allowed to send user {name} is already!' => '',
   'You cannot send a email to yourself!' => 'Vous ne pouvez pas envoyer un e-mail à vous-même.',
 );
--- messages/nl/views_mail_show.php
+++ messages/nl/views_mail_show.php
@@ -1,18 +1,17 @@
 <?php
-
-return [
-    'Delete discussion' => '',
-    '<strong>Confirm</strong> deleting conversation' => '<strong>Gesprek</strong> verwijderen',
-    '<strong>Confirm</strong> leaving conversation' => '<strong>Gesprek</strong> verlaten',
-    '<strong>Confirm</strong> message deletion' => '<strong>Bericht</strong> verwijderen',
-    'Add user' => 'Gebruiker toevoegen',
-    'Cancel' => 'Annuleren',
-    'Delete' => 'Verwijder',
-    'Do you really want to delete this conversation?' => 'Wil je dit gesprek echt verwijderen?',
-    'Do you really want to delete this message?' => 'Wil je dit bericht echt verwijderen?',
-    'Do you really want to leave this conversation?' => 'Wil je dit gesprek echt verlaten?',
-    'Leave' => 'Verlaat',
-    'Leave discussion' => 'Verlaat discussie',
-    'Send' => 'Versturen',
-    'There are no messages yet.' => 'Er zijn nog geen berichten.',
-];
+return array (
+  '<strong>Confirm</strong> deleting conversation' => '<strong>Gesprek</strong> verwijderen',
+  '<strong>Confirm</strong> leaving conversation' => '<strong>Gesprek</strong> verlaten',
+  '<strong>Confirm</strong> message deletion' => '<strong>Bericht</strong> verwijderen',
+  'Add user' => 'Gebruiker toevoegen',
+  'Cancel' => 'Annuleren',
+  'Delete' => 'Verwijder',
+  'Delete discussion' => 'Verwijder discussie',
+  'Do you really want to delete this conversation?' => 'Wilt u dit gesprek echt verwijderen?',
+  'Do you really want to delete this message?' => 'Wilt u dit bericht echt verwijderen?',
+  'Do you really want to leave this conversation?' => 'Wilt u dit gesprek echt verlaten?',
+  'Leave' => 'Verlaat',
+  'Leave discussion' => 'Verlaat discussie',
+  'Send' => 'Versturen',
+  'There are no messages yet.' => 'Er zijn nog geen berichten.',
+);
