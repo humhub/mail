6bea0c423aef3d3dfa6fbc588ef94c8b50dd8c91
HumHub Translations <info@humhub.com>
2020-06-29 15:12:58 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt_br/forms_InviteRecipientForm.php
+++ messages/pt_br/forms_InviteRecipientForm.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Recipient' => 'Destinatário',
-  'User {name} is already participating!' => '',
-  'You are not allowed to send user {name} is already!' => '',
+  'User {name} is already participating!' => 'O usuário {name} já está participando!',
+  'You are not allowed to send user {name} is already!' => 'Você ainda não pode enviar uma mensagem ao usuário {name}!',
   'You cannot send a email to yourself!' => 'Você NÃO PODE enviar um e-mail para si mesmo!',
 );
