71ac50e6a42d3ab6ec2f5616a3d412ec2eee11ba
Lucas Bartholemy <lucas@bartholemy.com>
2021-03-10 10:02:28 +0100

    Documentation improvements

--- docs/swagger/build.sh
+++ docs/swagger/build.sh
@@ -3,7 +3,7 @@
 for filename in *.yaml; do
     echo "--------- $filename ---------------------"
 
-    npx redoc-cli bundle --cdn -o ../html/$(basename "$filename" .yaml).html $filename
+    npx redoc-cli bundle --cdn -o $(basename "$filename" .yaml).html $filename
 
     # We need to use 'htm' as extension, since spectacle removes all other html files from target dir
     #/usr/bin/spectacle "$filename" -t "../html/" -f "$(basename "$filename" .yaml).htm"
