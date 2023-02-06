cb2d8a7628768f8e0fa22c82fba455db6acefef6
Yuriy Bakhtin <yurybakh@gmail.com>
2023-02-06 18:14:12 +0400

    Limit users list

--- widgets/ParticipantUserList.php
+++ widgets/ParticipantUserList.php
@@ -7,6 +7,7 @@ use humhub\libs\Html;
 use humhub\modules\mail\helpers\Url;
 use humhub\modules\mail\models\Message;
 use humhub\widgets\Link;
+use Yii;
 
 class ParticipantUserList extends Widget
 {
@@ -14,6 +15,8 @@ class ParticipantUserList extends Widget
      * @var Message
      */
     public $message;
+
+    public int $limit = 2;
 
     public function run()
     {
@@ -27,13 +30,18 @@ class ParticipantUserList extends Widget
 
     private function renderUserList(): string
     {
-        $users = $this->message->users;
+        $users = $this->message->getUsers()->limit($this->limit)->all();
         $userCount = count($users);
+        $userTotalCount = $this->message->getUsers()->count();
 
         $result = '';
         foreach ($users as $u => $user) {
             $result .= Html::encode($user->displayName);
             $result .= $u < $userCount - 1 ? ', ' : '';
+        }
+
+        if ($userTotalCount > $userCount) {
+            $result .= ', +' . Yii::t('MailModule.base', '{n,plural,=1{# other} other{# others}}', ['n' => $userTotalCount - $userCount]);
         }
 
         return $result;
