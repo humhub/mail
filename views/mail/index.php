3d1944c29395542d92210423f853425379a53c0b
Yuriy Bakhtin <yurybakh@gmail.com>
2023-03-01 11:04:12 +0400

    Use full width container on fluid themes

--- views/mail/index.php
+++ views/mail/index.php
@@ -3,13 +3,14 @@
 use humhub\modules\mail\assets\MailMessengerAsset;
 use humhub\modules\mail\models\UserMessage;
 use humhub\modules\mail\widgets\ConversationView;
+use humhub\modules\ui\view\helpers\ThemeHelper;
 
 /* @var $messageId int */
 /* @var $userMessages UserMessage[] */
 
 MailMessengerAsset::register($this);
 ?>
-<div class="container<?= $messageId ? ' mail-conversation-single-message' : '' ?>">
+<div class="<?= ThemeHelper::isFluid() ? 'container-fluid' : 'container' ?><?= $messageId ? ' mail-conversation-single-message' : '' ?>">
     <div class="row">
         <div class="col-md-4">
             <?= $this->render('_conversation_sidebar') ?>
