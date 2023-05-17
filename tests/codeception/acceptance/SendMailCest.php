cba2c4fb72a27aa60e4e9133d34567cc56e2e755
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2023-05-17 19:11:15 +0400

    Fix tests (#332)
    * Fix tests
    
    * Fix tests

--- tests/codeception/acceptance/SendMailCest.php
+++ tests/codeception/acceptance/SendMailCest.php
@@ -80,8 +80,9 @@ class SendMailCest
     private function createConversationByInbox(AcceptanceTester $I)
     {
         $I->wantTo('create another conversation');
-        $I->jsClick('#mail-conversation-overview #mail-conversation-create-button');
-        $I->waitForText('New message', null, '#globalModal');
+        $I->waitForElementVisible('#mail-conversation-create-button');
+        $I->jsClick('#mail-conversation-create-button');
+        $I->waitForText('New message', 10, '#globalModal');
         $this->sendMessage($I, 'Admin', 'Hi Admin!', 'Admin test message');
         $I->waitForText('Admin test message', null,'#mail-conversation-root');
         $I->see('Hi Admin!', '#mail-conversation-root');
