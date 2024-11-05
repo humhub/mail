599fd9859cf92f3fef56343d43df72dabb543511
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-05 14:33:07 -0500

    Update MessageUserJoined.php

--- models/states/MessageUserJoined.php
+++ models/states/MessageUserJoined.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2023 HumHub GmbH & Co. KG
@@ -16,10 +17,42 @@ namespace humhub\modules\mail\models\states;
 class MessageUserJoined extends AbstractMessageState
 {
     /**
+     * The default value for the 'enableMessageUserJoined' setting.
+     */
+    const DEFAULT_ENABLE_MESSAGE_USER_JOINED = true;
+
+    /**
+     * The key for the 'enableMessageUserJoined' setting.
+     */
+    const SETTING_ENABLE_MESSAGE_USER_JOINED = 'enableMessageUserJoined';
+
+    /**
      * @inheritdoc
      */
     public static function type(): int
     {
         return self::TYPE_USER_JOINED;
+    }
+
+    /**
+     * Checks if the MessageUserJoined state is enabled.
+     *
+     * @param array $settings The settings array, where the 'enableMessageUserJoined' setting is stored.
+     * @return bool
+     */
+    public static function isEnabled(array $settings): bool
+    {
+        return $settings[self::SETTING_ENABLE_MESSAGE_USER_JOINED] ?? self::DEFAULT_ENABLE_MESSAGE_USER_JOINED;
+    }
+
+    /**
+     * Checks if the MessageUserJoined state is disabled.
+     *
+     * @param array $settings The settings array, where the 'enableMessageUserJoined' setting is stored.
+     * @return bool
+     */
+    public static function isDisabled(array $settings): bool
+    {
+        return !self::isEnabled($settings);
     }
 }
