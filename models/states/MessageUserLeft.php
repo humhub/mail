bb46aedff4c650d1ba5d204a282a94428f3afc3d
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-11-05 14:33:41 -0500

    Update MessageUserLeft.php

--- models/states/MessageUserLeft.php
+++ models/states/MessageUserLeft.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2023 HumHub GmbH & Co. KG
@@ -16,10 +17,42 @@ namespace humhub\modules\mail\models\states;
 class MessageUserLeft extends AbstractMessageState
 {
     /**
+     * The default value for the 'enableMessageUserLeft' setting.
+     */
+    const DEFAULT_ENABLE_MESSAGE_USER_LEFT = true;
+
+    /**
+     * The key for the 'enableMessageUserLeft' setting.
+     */
+    const SETTING_ENABLE_MESSAGE_USER_LEFT = 'enableMessageUserLeft';
+
+    /**
      * @inheritdoc
      */
     public static function type(): int
     {
         return self::TYPE_USER_LEFT;
+    }
+
+    /**
+     * Checks if the MessageUserLeft state is enabled.
+     *
+     * @param array $settings The settings array, where the 'enableMessageUserLeft' setting is stored.
+     * @return bool
+     */
+    public static function isEnabled(array $settings): bool
+    {
+        return $settings[self::SETTING_ENABLE_MESSAGE_USER_LEFT] ?? self::DEFAULT_ENABLE_MESSAGE_USER_LEFT;
+    }
+
+    /**
+     * Checks if the MessageUserLeft state is disabled.
+     *
+     * @param array $settings The settings array, where the 'enableMessageUserLeft' setting is stored.
+     * @return bool
+     */
+    public static function isDisabled(array $settings): bool
+    {
+        return !self::isEnabled($settings);
     }
 }
